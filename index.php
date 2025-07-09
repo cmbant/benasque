f6098f472c1ef6734aff9216a015734761d95730
Antony Lewis <antony@cosmologist.info>
2025-07-09 10:40:18 +0100

    cachebust

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ try {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= htmlspecialchars($config['page_title']) ?></title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style.css?v=2">
 </head>
 
 <body>
--- talks.php
+++ talks.php
@@ -44,7 +44,7 @@ try {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Submitted Talks - Benasque 25</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style.css?v=2">
     <style>
         .talks-container {
             max-width: 1200px;
