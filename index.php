ebdc9880942a77d18379fa7ecf0cef12f0ac26d5
Antony Lewis <antony@cosmologist.info>
2025-07-11 10:16:00 +0100

    fix

--- index.php
+++ index.php
@@ -299,7 +299,6 @@ try {
     </div>
 
     <!-- MathJax for LaTeX rendering in arXiv titles -->
-    <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
     <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
     <script>
         window.MathJax = {
